25d09a46c5522ae792001ec16f69eefee27e693c
Jorge Ballesteros <joba1021@netcracker.com>
2025-05-09 19:09:10 -0500

    Envio de datos al servidor y creacion primer modelo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy import Column, Integer, String, Float
 
 app = Flask(__name__)
 
@@ -13,11 +14,24 @@ def index():
 def lista_productos():
     return render_template('lista_productos.html',titulo='Ver productos')
 
-@app.route('/formulario_producto')
+@app.route('/formulario_producto', methods=['GET','POST'])
 def formulario_producto():
+    if request.method == 'POST':
+        codigo = request.form.get('codigo')
+        print ("Entró por POST")
+        print(codigo)    
     return render_template('formulario_producto.html',titulo='Crear un producto')
-
 
 @app.route('/se_guardo')
 def se_guardo():
     return render_template('se_guardo.html',titulo='Se guardó!!!')
+
+
+class Productos():
+    id = Column(Integer, primary_key=True)
+    codigo = Column(String(9), unique=True, nullable=False)
+    descripcion = Column(String(300), unique=True, nullable=False)
+    valor_unitario = Column(Float(10,8))
+    unidad_medida = Column(String(3), unique=True, nullable=False)
+    cantidad_stock = Column(Float(10,8))
+    categoria = Column(Integer, nullable=False)
