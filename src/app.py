2ba0f42fecbd3c827ee477b69ee2f34afe4081e5
Jorge Ballesteros <joba1021@netcracker.com>
2025-05-16 06:30:50 -0500

    Conexion a la base de datos con sqlalchemy.orm creando las tablas desde el modelo

--- src/app.py
+++ src/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import pymysql
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+engine = create_engine("mysql+pymysql://root@localhost/factura243?charset=utf8mb4")
+
+connection = engine.connect()
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+Base = declarative_base()
+Base.metadata.bind = engine
+
+
 
 @app.route('/')
 def index():
@@ -27,11 +42,21 @@ def se_guardo():
     return render_template('se_guardo.html',titulo='Se guardó!!!')
 
 
-class Productos():
+class Productos(Base):
+    __tablename__ = "productos"
     id = Column(Integer, primary_key=True)
     codigo = Column(String(9), unique=True, nullable=False)
     descripcion = Column(String(300), unique=True, nullable=False)
     valor_unitario = Column(Float(10,8))
     unidad_medida = Column(String(3), unique=True, nullable=False)
     cantidad_stock = Column(Float(10,8))
-    categoria = Column(Integer, nullable=False)
+    categoria = Column(Integer, ForeignKey('categorias.id'), nullable=False)
+
+
+class Categorias(Base):
+    __tablename__ = "categorias"
+    id = Column(Integer, primary_key=True)
+    nombre_categoria = Column(String(300), unique=True, nullable=False)
+
+Base.metadata.create_all(engine)
+
