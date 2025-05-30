6b8165c3ac905c0c38f7a2c49e070db3186aa203
Jorge Ballesteros <joba1021@netcracker.com>
2025-05-29 19:15:06 -0500

    devolver errores mas puntuales a la vista

--- src/app.py
+++ src/app.py
@@ -19,8 +19,6 @@ session = Session()
 Base = declarative_base()
 Base.metadata.bind = engine
 
-
-
 @app.route('/')
 def index():
 
@@ -32,28 +30,30 @@ def lista_productos():
         productos = Productos.traer_productos()
         return render_template('lista_productos.html',titulo='Ver productos', productos = productos)
     except:
-        return render_template('lista_productos.html',titulo='Error de conexión a la base de datos')
-    
+        return render_template('lista_productos.html',titulo='Error de conexión a la base de datos')    
 
 @app.route('/formulario_producto', methods=['GET','POST'])
 def formulario_producto():
+    categorias = Categorias.traer_categorias() 
     if request.method == 'POST':
         codigo = request.form.get('codigo')
         descripcion = request.form.get('descripcion')
-        producto =  session.query(Productos).filter(Productos.descripcion == descripcion).first()
-        if producto:
-            return render_template('formulario_producto.html',titulo='Error:producto repetido')
         valor_unitario = request.form.get('valor_unitario')
-        cantidad_inventario = request.form.get('cantidad_inventario')        
+        cantidad_inventario = request.form.get('cantidad_inventario') 
         unidad_medida = request.form.get('unidad_medida')
         categoria = request.form.get('categoria')
-        producto = Productos(codigo,descripcion,valor_unitario,unidad_medida,cantidad_inventario,categoria)
+        producto_almacenar = Productos(codigo,descripcion,valor_unitario,unidad_medida,cantidad_inventario,categoria)        
+        producto_repetido =  session.query(Productos).filter(Productos.descripcion == descripcion).first()
+        if producto_repetido:
+            return render_template('formulario_producto.html'
+                                   ,titulo='Crear un producto'
+                                   ,errorProducto = "La descripción no se puede repetir"
+                                   ,categorias = categorias
+                                   ,producto_almacenar = producto_almacenar)
         try:
-            Productos.crear_producto(producto)
+            Productos.crear_producto(producto_almacenar)
         except:            
-            return render_template('formulario_producto.html',titulo='Error al registrar en la base de datos')
- 
-    categorias = Categorias.traer_categorias() 
+            return render_template('formulario_producto.html',titulo='Error al registrar en la base de datos',categorias = categorias) 
     return render_template('formulario_producto.html',titulo='Crear un producto',categorias = categorias)
 
 @app.route('/se_guardo')
@@ -102,8 +102,7 @@ class Categorias(Base):
 
     def traer_categorias():
         categorias = session.query(Categorias).all()
-        return categorias
-        
+        return categorias      
     
 
 Base.metadata.create_all(engine)
